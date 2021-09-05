252371d4718f58346bf5644a27aa39859ceee56e
bung87 <crc32@qq.com>
2021-09-05 11:39:54 +0800

    test import

--- tests/find_imports.spec.ts
+++ tests/find_imports.spec.ts
@@ -1,11 +1,30 @@
 import { test } from "tap";
 import { findImports } from "../src/find_imports";
 
-void test("find imports", async (t) => {
-  const imports = await findImports("fixtures/basic/*.ts", {
+void test("find imports export all", async (t) => {
+  const imports = await findImports("fixtures/export_all/*.ts", {
     packageImports: false,
     absoluteImports: true,
     relativeImports: true,
   });
-  t.ok(Object.keys(imports));
+  const expectKey = "fixtures/export_all/index.ts";
+  t.ok(expectKey in imports, `expect key ${expectKey} in imports`);
+  t.ok(
+    imports[expectKey].includes("./camelCaseModule"),
+    `expect values has './camelCaseModule'`
+  );
+});
+
+void test("find imports import one", async (t) => {
+  const imports = await findImports("fixtures/import_one/*.ts", {
+    packageImports: false,
+    absoluteImports: true,
+    relativeImports: true,
+  });
+  const expectKey = "fixtures/import_one/index.ts";
+  t.ok(expectKey in imports, `expect key ${expectKey} in imports`);
+  t.ok(
+    imports[expectKey].includes("./PascalCaseModule"),
+    `expect values has './PascalCaseModule'`
+  );
 });
